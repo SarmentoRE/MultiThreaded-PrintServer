a20a2fb029a89357159c9a16a126a9857f2f8974
jzheadley <headleyjz@vcu.edu>
2018-03-06 14:41:45 -0500

    setup command line args, makefile and header

--- main.c
+++ main.c
@@ -3,12 +3,20 @@
 #include "printQueue.h"
 int main(int argc, char* argv[])
 {
+    int producerThreads = atoi(argv[1]);
+    int consumerThreads = atoi(argv[2]);
+
+    printf("num producers:\t%i\n", producerThreads);
+    printf("num consumers:\t%i\n", consumerThreads);
+    return 1;
 }
 
 int userThreadFunc()
 {
+    return 1;
 }
 
 int printThreadFunc()
 {
+    return 1;
 }
